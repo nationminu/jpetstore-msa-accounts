9990f5f0ad98abf8da79971f8995ff38465091ce
ssong <ssong@10.64.23.124>
2021-05-26 15:17:54 +0900

    UPDATE permit

--- src/main/java/com/rock/jpetstore/config/WebSecurityConfig.java
+++ src/main/java/com/rock/jpetstore/config/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/customer/**","/live","/delay","/actuator/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .httpBasic();
