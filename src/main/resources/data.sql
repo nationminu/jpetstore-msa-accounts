91b77c039e6b06fc186d2b305035ed531da76e54
ssong <ssong@10.64.23.124>
2021-05-27 14:24:24 +0900

    UPDATE sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO signon(username,password,role,enabled)  VALUES('ACID','$2a$10$x/4hk/
 INSERT INTO account(userid,email,firstname,lastname,status,addr1,addr2,city,state,zip,country,phone) VALUES('j2ee','yourname@yourdomain.com','ABC', 'XYX', 'OK', '901 San Antonio Road', 'MS UCUP02-206', 'Palo Alto', 'CA', '94303', 'USA',  '555-555-5555');
 INSERT INTO account(userid,email,firstname,lastname,status,addr1,addr2,city,state,zip,country,phone) VALUES('ACID','acid@yourdomain.com','ABC', 'XYX', 'OK', '901 San Antonio Road', 'MS UCUP02-206', 'Palo Alto', 'CA', '94303', 'USA',  '555-555-5555');
 
-INSERT INTO profile(userid,banneropt,favcategory,langpref,mylistopt) VALUES('j2ee',1,'english','DOGS',1);
-INSERT INTO profile(userid,banneropt,favcategory,langpref,mylistopt) VALUES('ACID',1,'english','CATS',1);
+INSERT INTO profile(userid,banneropt,langpref,favcategory,mylistopt) VALUES('j2ee',1,'english','DOGS',1);
+INSERT INTO profile(userid,banneropt,langpref,favcategory,mylistopt) VALUES('ACID',1,'english','CATS',1);
